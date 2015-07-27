60faf5a9224884b731137d3b0854780cce484381
hspark <hspark@sogaeyo.com>
2015-07-27 05:11:00 +0000

    게시글 작성 및 저장 기능 추가하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
   root 'foods#posts'
-
 	get	"/:category" => "foods#posts_category"
-
   get 'foods/show/:id' => 'foods#show'
-
   get 'foods/write'
-
-  get 'foods/write_complete'
-
+  post 'foods/write_complete'
   get 'foods/edit'
-
   get 'foods/edit_complete'
-
   get 'foods/delete_complete'
 
   # The priority is based upon order of creation: first created -> highest priority.
